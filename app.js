cc917cdf5b5cc9899f86c468d3dce1fd40d26cba
christobeck <ga@christobeck.com>
2015-05-13 17:01:29 -0400

    cleaned up code a little

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-/* Tic-Tac-Toe Project by Chris Tobeck, WDI-BOS-07 at General Assembly. */
+/*   Tic-Tac-Toe Project by Chris Tobeck, WDI-BOS-07 at General Assembly.   */
 
 $(document).ready(function(){
+
+var player1Name=""
+var player2Name=""
 
 });
